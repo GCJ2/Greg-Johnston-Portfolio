e316a8abd76552641e73a4cff7d893260e3db66c
Greg Johnston <GCJohnston91@gmail.com>
2019-08-21 17:45:39 -0400

    Styling done to CTA

--- portfolio/src/components/Welcome.js
+++ portfolio/src/components/Welcome.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Profile from '../assets/ProfileCropped.jpg'
 
 const Welcome = (props) => {
 return(
-  <div>
+  <div className="welcome-container">
+    <div className="profile-wrapper">
+      <img className="profile-picture" src={Profile} alt="Greg Johnston Profile" />
+    </div>
     <h2>Greg Johnston</h2>
-    <p>Lambda School Student and Composer</p>
+    <p>Web Developer and Composer</p>
   </div>
   )
 }
