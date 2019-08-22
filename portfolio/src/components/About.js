ef77a5728f5125398bbf2e0a37b1e49397ded8d4
Greg Johnston <gcjohnston91@gmail.com>
2019-08-22 11:29:19 -0400

    .

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -5,8 +5,8 @@ return(
   <div>
     <h2>Hey there.</h2>
     <p>My name is Greg Johnston. I'm a student at Lambda School, a budding web developer, and a composer.</p>
-    <p>I love reading, writing music, coding, and creating</p>
-    <p>I'd love to be considered for your next project, be it in the world to tech, or music</p>
+    <p>I love reading, writing music, coding, and creating.</p>
+    <p>I'd love to be considered for your next project, be it in the world to tech, or music.</p>
   </div>
   )
 }
